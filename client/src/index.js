f1b271a08693d22cd7821e7ead570451f773f843
goran <goran.novacic@gmail.com>
2021-01-14 00:10:29 +0100

    #correct

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,9 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import App from './App';
-const render = (Component) => ReactDOM.render(
-	<AppContainer>
-		<App/>
-	</AppContainer>,
-	document.getElementById('root'),
-);
+import Routes from "./router/routes";
+
+ReactDOM.render(<Routes/>, document.getElementById('root'));
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
 	module.hot.accept('./App', () => { render(App); });
